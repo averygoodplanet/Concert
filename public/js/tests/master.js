01caf3154c300bd597d67bb1b644f7bdb0041dca
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-18 14:54:32 -0500

    fixed the for loop in createGA

--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -38,7 +38,7 @@ test('create General Admission section #ga', function(){
   //set options to ga Section
   $('#options').val('ga');
   //set number of seats to 40
-  $('#numSeats').val('40');
+  $('#numSeats').val('20');
   //click #create  ('Create Section')
   $('#create').trigger('click');
 
@@ -47,8 +47,8 @@ test('create General Admission section #ga', function(){
   //--#ga exists
   ok($('#ga').length, 'checking that #ga exists');
   //--#ga contains number of divs == 40
-  deepEqual($('#ga .seat').length, 40, 'checking that 40 seats created under #ga');
+  deepEqual($('#ga .seat').length, 20, 'checking that 20 seats created under #ga');
   //--#ga divs have border
   ok(_.every($('#ga div').css('border'), function(item) {return item === true;}), 'check that all ga divs have a border');
-  ok(_.every($('#ga > .seatRow'), function(row) {return $(row).children('div').length <= 10;}), 'check that each ga row only 10 seats wide');
+  ok(_.every($('#ga > .seatRow'), function(row) {return $(row).children('div').length <= 15;}), 'check that each ga row only 15 seats wide');
 });
