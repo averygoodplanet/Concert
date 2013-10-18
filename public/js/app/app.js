5dc392e4d5dcfb37cbb395a6be77470187cc8e48
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-18 14:34:19 -0500

    createGA() makes seats for full row and unfullrow

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -26,11 +26,37 @@ function clickCreate() {
 function createGA() {
   //Because this function was called, I already know that general admission section was selected.
   var numberOfSeats = parseInt($('#numSeats').val());
-  var seatsPerRow = 20;
+  var seatsPerRow = 15;
   var seatInfo = seatMath(numberOfSeats, seatsPerRow);
   // pass seatMath(numberOfSeats, seatsPerRow)
   // seatMath returns {fullrows: 2, unfullrow: 1}
-  debugger;
+  var numberFullRows = seatInfo.fullrows;
+  var numberInUnfullRow = seatInfo.unfullrow;
+  var fullrow = '<div class = "seatRow"></div>';
+  var $fullRow = $(fullrow);
+  //add seats, where seats have class .seat to fullRow;
+  for(var i = 0; i < seatsPerRow; i++){
+    var seat = '<div class = "seat"></div>';
+    var $seat = $(seat);
+    $fullRow.append($seat);
+  }
+
+  //for number of full rows, add full rows to #ga
+  for(var i = 0; i < numberFullRows; i++){
+    var $row = $fullRow;
+    $('#ga').append($row);
+  }
+
+  if(numberInUnfullRow > 0){
+    var unfullrow = '<div class = "seatRow unfull"></div>';
+    var $unfullrow = $(unfullrow);
+    for(var j = 0; j < numberInUnfullRow; j++){
+      var newSeat = '<div class = "seat"></div>';
+      var $newSeat = $(newSeat);
+      $unfullrow.append($newSeat);
+    }
+    $('#ga').append($unfullrow);
+  }
 }
 
 
