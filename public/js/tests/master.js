358695e5370ee67f946f02ceb0f0d725aef85f9b
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-18 12:01:29 -0500

    Wrote test for general admission section.

--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -10,8 +10,8 @@ function teardownTest(){
 
 }
 
-test("create VIP section #vip", function(){
-  expect(3);
+test('create VIP section #vip', function(){
+  expect(4);
 
   //set options to VIP Section
   $('#options').val('vip');
@@ -29,4 +29,26 @@ test("create VIP section #vip", function(){
   //--#vip divs have border
   ok(_.every($('#vip div').css('border'), function(item) {return item === true;}), 'check that all vip divs have a border');
   ok(_.every($('#vip > .seatRow'), function(row) {return $(row).children('div').length <= 10;}), 'check that each vip row only 10 seats wide');
+});
+
+
+test('create General Admission section #ga', function(){
+  expect(4);
+
+  //set options to ga Section
+  $('#options').val('ga');
+  //set number of seats to 40
+  $('#numSeats').val('40');
+  //click #create  ('Create Section')
+  $('#create').trigger('click');
+
+  //do checks to verify it was created;
+
+  //--#ga exists
+  ok($('#ga').length, 'checking that #ga exists');
+  //--#ga contains number of divs == 40
+  deepEqual($('#ga .seat').length, 40, 'checking that 40 seats created under #ga');
+  //--#ga divs have border
+  ok(_.every($('#ga div').css('border'), function(item) {return item === true;}), 'check that all ga divs have a border');
+  ok(_.every($('#ga > .seatRow'), function(row) {return $(row).children('div').length <= 10;}), 'check that each ga row only 10 seats wide');
 });
